486d587e30fae424f331d873bff7b726c9ad2425
Lewin <57636993+lewinkoon@users.noreply.github.com>
2024-03-05 14:34:41 +0100

    export `.csv` file

--- core/export.py
+++ core/export.py
@@ -1,3 +1,4 @@
+import csv
 import numpy as np
 import os
 from core.api import parse_dicom
@@ -17,17 +18,28 @@ def export():
     rl = [img["val"] for img in data if img["axis"] == "RL" and img["time"] == 0]
     ap = [img["val"] for img in data if img["axis"] == "AP" and img["time"] == 0]
 
+    table = []
     for z, (imgy, imgx, imgz) in enumerate(zip(fh, rl, ap)):
         for index, (pxlx, pxly, pxlz) in enumerate(
             zip(imgy.flatten(), imgx.flatten(), imgz.flatten())
         ):
-            table = {}
-            table["x"] = np.unravel_index(index, (128, 128))[0]
-            table["y"] = np.unravel_index(index, (128, 128))[1]
-            table["z"] = z
-            table["vx"] = pxlx
-            table["vy"] = pxly
-            table["vz"] = pxlz
-            print(table)
-            break
-        break
+            row = {}
+            row["x"] = np.unravel_index(index, (128, 128))[0]
+            row["y"] = np.unravel_index(index, (128, 128))[1]
+            row["z"] = z
+            row["vx"] = pxlx
+            row["vy"] = pxly
+            row["vz"] = pxlz
+            table.append(row)
+
+    print(table[0])
+
+    fields = table[0].keys()
+    path = "output.csv"
+
+    with open(path, mode="w", newline="") as file:
+        writer = csv.DictWriter(file, fieldnames=fields)
+        writer.writeheader()
+
+        for row in table:
+            writer.writerow(row)
