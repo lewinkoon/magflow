6dc1cffc42be1558daace179632491ba5624a035
Lewin <57636993+lewinkoon@users.noreply.github.com>
2024-03-05 15:00:29 +0100

    refactor export function code

--- core/api.py
+++ core/api.py
@@ -1,3 +1,4 @@
+import csv
 import numpy as np
 import os
 from pydicom import dcmread
@@ -19,6 +20,54 @@ def parse_dicom(file):
                 ds.pixel_array, ds
             )  # convert pixel values to velocity values
             return tags
+
+
+def read_files(folder):
+    data = []
+    for axis in ("FH", "RL", "AP"):
+        folder_path = f"{folder}/{axis}"
+        for file in os.listdir(folder_path):
+            file_path = os.path.join(folder_path, file)
+            if parse_dicom(file_path) is not None:
+                data.append(parse_dicom(file_path))
+
+    res = sorted(data, key=lambda x: x["loc"])
+    return res
+
+
+def tabulate(data):
+    # filter images by axis and time
+    fh = [img["val"] for img in data if img["axis"] == "FH" and img["time"] == 0]
+    rl = [img["val"] for img in data if img["axis"] == "RL" and img["time"] == 0]
+    ap = [img["val"] for img in data if img["axis"] == "AP" and img["time"] == 0]
+
+    # convert data into tabular data
+    res = []
+    for z, (imgy, imgx, imgz) in enumerate(zip(fh, rl, ap)):
+        for index, (pxlx, pxly, pxlz) in enumerate(
+            zip(imgy.flatten(), imgx.flatten(), imgz.flatten())
+        ):
+            row = {}
+            row["x"] = np.unravel_index(index, (128, 128))[0]
+            row["y"] = np.unravel_index(index, (128, 128))[1]
+            row["z"] = z
+            row["vx"] = pxlx
+            row["vy"] = pxly
+            row["vz"] = pxlz
+            res.append(row)
+
+    return res
+
+
+def write_csv(data, path):
+    # export data as csv
+    fields = data[0].keys()
+    path = "output.csv"
+    with open(path, mode="w", newline="") as file:
+        writer = csv.DictWriter(file, fieldnames=fields)
+        writer.writeheader()
+        for row in data:
+            writer.writerow(row)
 
 
 def get_velocities(data, time):
--- core/export.py
+++ core/export.py
@@ -1,45 +1,15 @@
 import csv
 import numpy as np
 import os
-from core.api import parse_dicom
+from core.api import read_files, tabulate, write_csv
 
 
 def export():
-    # read all the files
-    data = []
-    for axis in ("FH", "RL", "AP"):
-        folder = f"files/{axis}"
-        for file in os.listdir(folder):
-            path = os.path.join(folder, file)
-            if parse_dicom(path) is not None:
-                data.append(parse_dicom(path))
+    # create a list of dictionries with the read data
+    data = read_files("files")
 
-    fh = [img["val"] for img in data if img["axis"] == "FH" and img["time"] == 0]
-    rl = [img["val"] for img in data if img["axis"] == "RL" and img["time"] == 0]
-    ap = [img["val"] for img in data if img["axis"] == "AP" and img["time"] == 0]
+    # convert image series into tabular velocity field
+    table = tabulate(data)
 
-    table = []
-    for z, (imgy, imgx, imgz) in enumerate(zip(fh, rl, ap)):
-        for index, (pxlx, pxly, pxlz) in enumerate(
-            zip(imgy.flatten(), imgx.flatten(), imgz.flatten())
-        ):
-            row = {}
-            row["x"] = np.unravel_index(index, (128, 128))[0]
-            row["y"] = np.unravel_index(index, (128, 128))[1]
-            row["z"] = z
-            row["vx"] = pxlx
-            row["vy"] = pxly
-            row["vz"] = pxlz
-            table.append(row)
-
-    print(table[0])
-
-    fields = table[0].keys()
-    path = "output.csv"
-
-    with open(path, mode="w", newline="") as file:
-        writer = csv.DictWriter(file, fieldnames=fields)
-        writer.writeheader()
-
-        for row in table:
-            writer.writerow(row)
+    # export data as csv
+    write_csv(table, "output.csv")
