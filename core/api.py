34ac8f768816e4e5ca47ef00cf7a1e2c4ca9884c
Lewin <57636993+lewinkoon@users.noreply.github.com>
2024-03-18 13:56:27 +0100

    mask velocity field with segmentation

--- core/api.py
+++ core/api.py
@@ -1,55 +1,46 @@
 import csv
-from itertools import groupby
 import numpy as np
 import os
 from pydicom import dcmread
 from pydicom.pixel_data_handlers.util import apply_modality_lut
 
 
-def parse_dicom(file):
-    # extract image and metadata from a dicom file as a dictionary
-    with open(file, "rb") as file:
-        ds = dcmread(file)
-        if ds[0x0008, 0x0008].value[2] == "PHASE CONTRAST M":
-            tags = {}
-            tags["axis"] = ds[0x0008, 0x103E].value
-            tags["num"] = ds[0x0020, 0x0013].value  # get instance number
-            tags["loc"] = ds[0x0020, 0x1041].value  # get slice location
-            tags["time"] = ds[0x0018, 0x1060].value
-            tags["spacing"] = ds[0x0028, 0x0030].value
-            tags["height"] = ds[0x0018, 0x0088].value
-            # tags["img"] = ds.pixel_array
-            tags["val"] = apply_modality_lut(
-                ds.pixel_array, ds
-            )  # convert pixel values to velocity values
-            return tags
+def parse_dicom(axis):
+    res = []
+    folder_path = f"files/{axis}"
+    for file in os.listdir(folder_path):
+        file_path = os.path.join(folder_path, file)
+        slice = {}
+        with open(file_path, "rb") as file:
+            ds = dcmread(file)
+            slice["axis"] = ds[0x0008, 0x103E].value
+            slice["num"] = ds[0x0020, 0x0013].value  # get instance number
+            slice["spacing"] = ds[0x0028, 0x0030].value
+            slice["pxl"] = ds.pixel_array
+            if ds[0x0008, 0x0008].value[2] == "PHASE CONTRAST M":
+                slice["height"] = ds[0x0018, 0x0088].value
+                slice["loc"] = ds[0x0020, 0x1041].value  # get slice location
+                slice["time"] = ds[0x0018, 0x1060].value
+                slice["val"] = apply_modality_lut(
+                    ds.pixel_array, ds
+                )  # convert pixel values to velocity values
+            res.append(slice)
+    return res
 
 
-def read_files(folder):
-    data = []
-    for axis in ("FH", "RL", "AP"):
-        folder_path = f"{folder}/{axis}"
-        for file in os.listdir(folder_path):
-            file_path = os.path.join(folder_path, file)
-            if parse_dicom(file_path) is not None:
-                data.append(parse_dicom(file_path))
-    return data
-
-
-def write_csv(data, time):
-    # get pixel spacing
-    spcx = data[0]["spacing"][0]
-    spcy = data[0]["spacing"][1]
-    spcz = data[0]["height"]
-
+def write_csv(fh, rl, ap, mk, spcx, spcy, spcz, time):
     # filter images by axis and time
-    fh = [img["val"] for img in data if img["axis"] == "FH" and img["time"] == time]
-    rl = [img["val"] for img in data if img["axis"] == "RL" and img["time"] == time]
-    ap = [img["val"] for img in data if img["axis"] == "AP" and img["time"] == time]
+    fh = [slice["val"] for slice in fh if slice["time"] == time]
+    rl = [slice["val"] for slice in rl if slice["time"] == time]
+    ap = [slice["val"] for slice in ap if slice["time"] == time]
+    mk = [slice["pxl"] for slice in mk]
 
     # convert data into tabular data
     res = []
-    for z, (imgx, imgy, imgz) in enumerate(zip(ap, fh, rl)):
+    for z, (imgx, imgy, imgz, imgm) in enumerate(zip(ap, fh, rl, mk)):
+        imgx[imgm] = 0
+        imgy[imgm] = 0
+        imgz[imgm] = 0
         for index, (pxlx, pxly, pxlz) in enumerate(
             zip(imgx[::-1].flatten(), imgy[::-1].flatten(), imgz[::-1].flatten())
         ):
@@ -84,25 +75,3 @@ def get_velocities(data, time):
     ap = np.array([img["val"] for img in data["AP"][:, time]])
 
     return np.stack((fh, rl, ap), axis=-1)
-
-    # # create a three-dimensional grid filled with cells values from images
-    # arr = get_velocities(data, 15)
-    # grid = pv.ImageData()
-    # grid.dimensions = (144, 144, 80)
-    # grid.origin = (0, 0, 0)
-    # grid.spacing = (1.875, 1.875, 2.5)
-    # grid["velocity"] = arr.reshape((-1, 3))
-    # print(grid)
-    # orto = grid.slice_orthogonal(135, 135, 52.5)
-
-    # clip the grid with the segmented ct
-    # aorta = pv.read("files/main.stl")
-    # clipped = grid.clip_surface(aorta, invert=True, crinkle=False)
-
-    # # preview the problem
-    # p = pv.Plotter()
-    # p.add_mesh(orto, show_edges=True, component=1, cmap="jet")
-    # # p.add_mesh(streamlines.tube(radius=0.1))
-    # # p.add_mesh(aorta, show_edges=True)
-    # p.show_axes()
-    # p.show()
--- main.py
+++ main.py
@@ -1,9 +1,54 @@
+import os
+from functools import partial
+import multiprocessing
+import numpy as np
 import sys
-from core.gui import gui
-from core.export import export
+from tqdm import tqdm
+from core.api import parse_dicom, write_csv
+
+
+def export():
+    # create a list of dictionries with the read data
+    fh = parse_dicom("FH")
+    rl = parse_dicom("RL")
+    ap = parse_dicom("AP")
+    mk = parse_dicom("SG")
+
+    # get pixel spacing
+    timeframes = sorted(set(item["time"] for item in fh))
+    spcx = fh[0]["spacing"][0]
+    spcy = fh[0]["spacing"][1]
+    spcz = fh[0]["height"]
+
+    # # export csv files
+    # for t in timeframes:
+    #     write_csv(fh, rl, ap, mk, spcx, spcy, spcz, t)
+
+    # export to csv with multiprocessing
+    with tqdm(total=len(timeframes)) as pbar:
+        worker = partial(write_csv, fh, rl, ap, mk, spcx, spcy, spcz)
+        with multiprocessing.Pool() as pool:
+            for _ in pool.imap_unordered(worker, timeframes):
+                pbar.update()
+
+
+def gui():
+    # fill the dictionary with data
+    data = {"FH": [], "RL": [], "AP": []}
+    for key in data:
+        folder = f"files/{key}"
+        files = os.listdir(folder)
+        series = [
+            parse_dicom(file, folder)
+            for file in files
+            if parse_dicom(file, folder) is not None
+        ]
+        series = sorted(series, key=lambda x: x["num"])
+        data[key] = np.reshape(np.array(series), (40, 24))
+
 
 if __name__ == "__main__":
     if len(sys.argv) > 1 and sys.argv[1] == "--export":
         export()
     else:
-        gui()
+        export()
