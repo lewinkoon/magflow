affcd6e715e423851eafffc30ee9881d49516572
Lewin <57636993+lewinkoon@users.noreply.github.com>
2025-02-05 14:47:11 +0100

    refactor: enhance clean command

--- magflow/commands/clean.py
+++ magflow/commands/clean.py
@@ -7,29 +7,34 @@ app = typer.Typer()
 
 
 @app.command("clean")
-def clean():
+def clean(
+    all: bool = typer.Option(
+        False, "--all", help="Clean both files and output directories if provided"
+    ),
+):
     """
     Remove exported data.
     """
-    path = "output"
+    paths = ["output", "files"] if all else ["output"]
 
-    # check first if path exists
-    if not os.path.exists(path):
-        logger.info("Output files not exported yet.")
-    else:
-        try:
-            for filename in os.listdir(path):
-                file_path = os.path.join(path, filename)
-                try:
-                    if os.path.isfile(file_path) or os.path.islink(file_path):
-                        os.remove(file_path)
-                        logger.info(f"Removed {file_path}")
-                    elif os.path.isdir(file_path):
-                        os.rmdir(file_path)
-                        logger.info(f"Removed directory {file_path}")
-                except Exception as e:
-                    logger.error(f"Error while deleting {file_path}: {e}")
-            os.rmdir(path)
-            logger.info(f"Removed {path}")
-        except Exception as e:
-            logger.error(f"Output directory cannot be removed: {e}")
+    for path in paths:
+        # check first if path exists
+        if not os.path.exists(path):
+            logger.info(f"{path} files not exported yet.")
+        else:
+            try:
+                for filename in os.listdir(path):
+                    file_path = os.path.join(path, filename)
+                    try:
+                        if os.path.isfile(file_path) or os.path.islink(file_path):
+                            os.remove(file_path)
+                            logger.info(f"Removed {file_path}")
+                        elif os.path.isdir(file_path):
+                            os.rmdir(file_path)
+                            logger.info(f"Removed directory {file_path}")
+                    except Exception as e:
+                        logger.error(f"Error while deleting {file_path}: {e}")
+                os.rmdir(path)
+                logger.info(f"Removed {path}")
+            except Exception as e:
+                logger.error(f"{path} directory cannot be removed: {e}")
