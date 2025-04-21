26ec518a4f8c51f187b21d94ee3ed1f015d6cf7c
Lewin <57636993+lewinkoon@users.noreply.github.com>
2025-04-21 09:57:19 +0200

    refactor: remove patch command

--- magflow/cli.py
+++ magflow/cli.py
@@ -4,7 +4,6 @@ from .commands.build import app as build_app
 from .commands.check import app as check_app
 from .commands.clean import app as clean_app
 from .commands.load import app as load_app
-from .commands.patch import app as patch_app
 
 # Initialize the main app
 app = typer.Typer(
@@ -16,7 +15,6 @@ app.add_typer(build_app)
 app.add_typer(check_app)
 app.add_typer(clean_app)
 app.add_typer(load_app)
-app.add_typer(patch_app)
 
 
 if __name__ == "__main__":
