95389fe6a543e6b7ca647720a2c66ac967b7221b
Lewin <57636993+lewinkoon@users.noreply.github.com>
2025-04-22 14:55:52 +0200

    refactor: simplify path handling

--- magflow/commands/serve.py
+++ magflow/commands/serve.py
@@ -6,17 +6,18 @@ from typing_extensions import Annotated
 
 app = typer.Typer()
 
+
 @app.command("serve")
 def serve(port: Annotated[int, typer.Option(help="Port to run the server on.")] = 8501):
     """
     Start the web interface using streamlit.
     """
-    # Get the commands directory
-    commands_dir = os.path.dirname(os.path.abspath(__file__))
-    # Go one level up to get the package root directory
-    package_root = os.path.dirname(commands_dir)
-    # Path to ui.py in the package root
-    ui_path = os.path.join(package_root, "ui.py")
 
     # Run streamlit with the path to ui.py
-    cli.main_run(["C:/Users/Luis/Repositories/magflow/magflow/ui.py", "--server.port", str(port)])
+    cli.main_run(
+        [
+            r"C:/Users/Luis/Repositories/magflow/magflow/ui.py",
+            "--server.port",
+            str(port),
+        ]
+    )
