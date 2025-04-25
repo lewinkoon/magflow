d73666c921d0532a3ae6af36ab1b3c892a0ce0f2
Lewin <57636993+lewinkoon@users.noreply.github.com>
2025-04-25 09:14:42 +0200

    refactor: replace load command with export command

--- magflow/cli.py
+++ magflow/cli.py
@@ -3,7 +3,7 @@ import typer
 from .commands.build import app as build_app
 from .commands.check import app as check_app
 from .commands.clean import app as clean_app
-from .commands.load import app as load_app
+from .commands.export import app as export_app
 from .commands.serve import app as serve_app
 from .commands.visualize import app as visualize_app
 
@@ -16,7 +16,7 @@ app = typer.Typer(
 app.add_typer(build_app)
 app.add_typer(check_app)
 app.add_typer(clean_app)
-app.add_typer(load_app)
+app.add_typer(export_app)
 app.add_typer(serve_app)
 app.add_typer(visualize_app)
 
